65e6e9925a62b53b16645bd0288602a645b6e832
Hugo Leroux <hugolerouxmusic@gmail.com>
2023-05-23 16:02:23 +0200

    Action.js rework

--- src/views/Actions.js
+++ src/views/Actions.js
@@ -1,23 +1,16 @@
 import eyeBlueIcon from "../assets/svg/eye_blue.js"
-import downloadBlueIcon from "../assets/svg/download_blue.js"
+// import downloadBlueIcon from "../assets/svg/download_blue.js"
 
 export default (billUrl) => {
-  if(billUrl.substr(-5) !== '/null') {
-    return (
-      `<div class="icon-actions">
-        <div id="eye" data-testid="icon-eye" data-bill-url=${billUrl}>
-        ${eyeBlueIcon}
-        </div>
-      </div>`
-    )
-  } else {
-    return (
-      `<div class="icon-actions">
-        <div id="eye-grey" data-testid="icon-eye-grey">
-        ${eyeBlueIcon}
-        </div>
-      </div>`
-    )
-  }
-  
+  const divData = billUrl.substr(-5) !== '/null' ?
+    `<div id="eye" data-testid="icon-eye" data-bill-url=${billUrl}>`
+    :
+      '<div id="eye-grey" data-testid="icon-eye-grey">'
+  return (
+    `<div class="icon-actions">
+      ${divData}
+      ${eyeBlueIcon}
+      </div>
+    </div>`
+  )
 }
