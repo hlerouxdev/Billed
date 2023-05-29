d60da073be32dac423a0385e81e92a6d3c6a154a
Hugo Leroux <hugolerouxmusic@gmail.com>
2023-05-29 13:19:43 +0200

    multibles bills on dashboard clickable

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -146,11 +146,10 @@ export default class {
     }
 
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#open-bill${bill.id}`).off().click((e) => this.handleEditTicket(e, bill, bills))
     })
 
     return bills
-
   }
 
   getBillsAllUsers = () => {
